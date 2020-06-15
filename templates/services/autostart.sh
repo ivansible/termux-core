0ba899cb18bd52d438d4d9839fc80571d9d5ab09
Ivan Andreev <ivandeex@gmail.com>
2020-06-15 03:38:50 +0300

    fix autostart problems with sshd

--- templates/services/autostart.sh
+++ templates/services/autostart.sh
@@ -2,4 +2,6 @@
 echo "$(date) :: $(df -h {{ root_dir }} |tail -1)" | tee -a {{ boot_log }}
 . $PREFIX/etc/profile
 {{ wakelock_comment }}termux-wake-lock
-{{ ubuntu_ssh_comment }}{{ ubuntu_start }} "/etc/init.d/ssh start" & >/dev/null 2>&1
+{{ ubuntu_ssh_comment }}nohup {{ ubuntu_start }} "/etc/init.d/ssh start" >/dev/null 2>&1 &
+{# workaround for runsvdir not working #}
+{{ termux_ssh_comment }}sleep 1; pgrep -f termux/files/usr/bin/sshd >/dev/null || sshd
