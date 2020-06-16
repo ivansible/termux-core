fe70f2dc12a36256138493989daf3e8bc27bcc49
Ivan Andreev <ivandeex@gmail.com>
2020-06-16 03:21:25 +0300

    setup shared memory

--- templates/services/autostart.sh
+++ templates/services/autostart.sh
@@ -2,6 +2,8 @@
 echo "$(date) :: $(df -h {{ root_dir }} |tail -1)" | tee -a {{ boot_log }}
 . $PREFIX/etc/profile
 {{ wakelock_comment }}termux-wake-lock
+{# shmem requires root! #}
+{{ shmem_comment }}su -c "mkdir -p /dev/shm; chmod 1777 /dev/shm"
 {{ ubuntu_ssh_comment }}nohup {{ ubuntu_start }} "/etc/init.d/ssh start" >/dev/null 2>&1 &
-{# workaround for runsvdir not working #}
+{# workaround for non-working runsvdir #}
 {{ termux_ssh_comment }}sleep 1; pgrep -f termux/files/usr/bin/sshd >/dev/null || sshd
