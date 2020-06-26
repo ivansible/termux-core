620d7bd6ae26043c00daceae0a926ce2ffab31d8
Ivan Andreev <ivandeex@gmail.com>
2020-06-26 17:54:26 +0300

    bind sdcard and more into proot

--- templates/ubuntu/start-script.sh
+++ templates/ubuntu/start-script.sh
@@ -7,8 +7,22 @@ command+=" -0"
 command+=" -r {{ ubuntu_fs }}"
 command+=" -b /dev"
 command+=" -b /proc"
+{% if termux_ubuntu_extra_mounts |bool %}
+# bind home directory
 command+=" -b {{ termux_root_dir }}/home:/root"
 command+=" -w /root"
+# bind sdcards
+command+=" -b /storage/emulated/0:/mnt/internal"
+command+=" -b {{ termux_sdcard_abspath }}:/mnt/sdcard"
+# enable running termux binaries from proot
+{# /data   has termux binaries /data/data/com.termux/files/usr/bin/* #}
+{# /system has termux linker   /system/bin/linker64 #}
+{# /apex   has andoird shared libraries /apex/com.android.runtime/lib64/*.so #}
+command+=" -b /sys"
+command+=" -b /data"
+command+=" -b /system"
+command+=" -b /apex"
+{% endif %}
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
 command+=" PATH={{ termux_ubuntu_path }}"
