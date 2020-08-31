5cadc8f86a7b55a70386219de7bc5c0d80dd9269
Ivan Andreev <ivandeex@gmail.com>
2020-08-31 16:50:15 +0300

    configure ips on loopback device

--- templates/services/autostart.sh
+++ templates/services/autostart.sh
@@ -7,3 +7,6 @@ echo "$(date) :: $(df -h {{ root_dir }} |tail -1)" | tee -a {{ boot_log }}
 {{ ubuntu_ssh_comment }}nohup {{ ubuntu_start }} "/etc/init.d/ssh start" >/dev/null 2>&1 &
 {# workaround for non-working runsvdir #}
 {{ termux_ssh_comment }}sleep 1; pgrep -f termux/files/usr/bin/sshd >/dev/null || sshd
+{% for ip in termux_loopback_ips %}
+sudo ip -{{ 6 if ':' in ip else 4 }} addr replace {{ ip }} dev lo
+{% endfor %}
